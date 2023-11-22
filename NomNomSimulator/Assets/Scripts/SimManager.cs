5fbef21fe2f3fce59782ea11f9fb13d3001c6637
danfupo03 <danfupo@outlook.com>
2023-11-22 11:38:25 -0600

    feat(Assets): Import more assets

--- NomNomSimulator/Assets/Scripts/SimManager.cs
+++ NomNomSimulator/Assets/Scripts/SimManager.cs
@@ -4,34 +4,5 @@ using UnityEngine;
 
 public class SimManager : MonoBehaviour
 {
-    [SerializeField] Collector collector;
-
-    public void OnEnable()
-    {
-        TimeManager.OnMinuteChanged += TimeCheck;
-    }
-
-    public void OnDisable()
-    {
-        TimeManager.OnMinuteChanged -= TimeCheck;
-    }
-
-    private void TimeCheck()
-    {
-        if (TimeManager.Hour == 0)
-        {
-            switch (TimeManager.Minute)
-            {
-                case 2:
-                    collector.Move(0, 0);
-                    break;
-                case 3:
-                    collector.Move(0, 1);
-                    break;
-                case 4:
-                    collector.Move(0, 2);
-                    break;
-            }
-        }
-    }
+        
 }
