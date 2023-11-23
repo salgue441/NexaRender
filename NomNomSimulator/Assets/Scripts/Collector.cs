a0fde25930faa5ffa268dff4f860a81020e1d4e2
salgue441 <carlossalguero441@gmail.com>
2023-11-23 12:48:47 -0600

    feat(AgentMovement): agent now moves in light speed Refactor to the main classes of the project, allowing the agents to move from step 0

--- NomNomSimulator/Assets/Scripts/Collector.cs
+++ NomNomSimulator/Assets/Scripts/Collector.cs
@@ -1,28 +1,67 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using System;
-using System.Threading;
+using System.Collections;
 
+/// <summary>
+/// Manages the behavior of a collector agent, including its appearance
+/// and movement within the simulation environment.
+/// </summary>
 public class Collector : MonoBehaviour
 {
     public AgentModel agent;
 
+    // Constructor
+    /// <summary>
+    /// Creates a new instance of Collector:: class.
+    /// </summary>
+    /// <param name="id">The ID of the agent to be spawned</param>
     public Collector(string id)
     {
         this.agent = new AgentModel();
         this.agent.id = id;
-        this.agent.type = "collector";
+        this.agent.type = "collector_";
     }
+
+    /// <summary>
+    /// Handles the spawning of the agent in the simulat    ion environment.
+    /// </summary>
+    /// <param name="x">The x-coordinate of the agent's spawn location</param>
+    /// <param name="z">The z-coordinate of the agent's spawn location</param>
     public void Appearance(int x, int z)
     {
-        // Aparecer en la posición inicial
-        transform.position = new Vector3(x, 0.6f, z);
+        transform.position = new(x, 0.6f, z);
     }
 
+    /// <summary>
+    /// Handles the movement of the agent in the simulation environment.
+    /// </summary>
+    /// <param name="x">The x-coordinate of the agent's destination</param>
+    /// <param name="z">The z-coordinate of the agent's destination</param>
     public void Move(int x, int z)
     {
-        transform.position = new Vector3(x, 0.6f, z);
+        StartCoroutine(MoveToPosition(new(x, 0.6f, z), 10f));
     }
 
+    /// <summary>
+    /// Moves the agent to a specified position in the simulation environment.
+    /// Allows for the agent to move in a smooth, linear fashion.
+    /// </summary>
+    /// <param name="target">The position to move the agent to</param>
+    /// <param name="duration">The duration of the movement</param>
+    /// <returns></returns>
+    IEnumerator MoveToPosition(Vector3 target, float duration)
+    {
+        float time = 0;
+        Vector3 startPosition = transform.position;
+
+        while (time < duration)
+        {
+            transform.position = Vector3.Lerp(startPosition, target,
+                time / duration);
+
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = target;
+    }
 }
--- NomNomSimulator/Assets/Scripts/Explorer.cs
+++ NomNomSimulator/Assets/Scripts/Explorer.cs
@@ -1,27 +1,62 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using System;
-using System.Threading;
+using System.Collections;
 
+/// <summary>
+/// Manages the behavior of an explorer agent in the simulation.
+/// </summary>
 public class Explorer : MonoBehaviour
 {
     private int id;
 
-    public Explorer(int id)
+    /// <summary>
+    /// Initializes the Explorer with an ID.
+    /// </summary>
+    /// <param name="id">The unique identifier for the explorer.</param>
+    public void Initialize(int id)
     {
         this.id = id;
     }
+
+    /// <summary>
+    /// Sets the initial appearance of the explorer at a specific position.
+    /// </summary>
+    /// <param name="x">The x-coordinate of the explorer's initial position.</param>
+    /// <param name="z">The z-coordinate of the explorer's initial position.</param>
     public void Appearance(int x, int z)
     {
-        // Aparecer en la posición inicial
         transform.position = new Vector3(x, 0.6f, z);
     }
 
+    /// <summary>
+    /// Moves the explorer to a new position.
+    /// </summary>
+    /// <param name="x">The x-coordinate of the new position.</param>
+    /// <param name="z">The z-coordinate of the new position.</param>
     public void Move(int x, int z)
     {
-        // Moverse a la posición indicada
-        transform.position = new Vector3(x, 0.6f, z);
+        StartCoroutine(MoveToPosition(new Vector3(x, 0.6f, z), 10f));
     }
 
+    /// <summary>
+    /// Coroutine to move the explorer to a specified position in the simulation environment.
+    /// </summary>
+    /// <param name="target">The position to move the explorer to.</param>
+    /// <param name="duration">The duration of the movement.</param>
+    /// <returns></returns>
+    IEnumerator MoveToPosition(Vector3 target, float duration)
+    {
+        float time = 0;
+        Vector3 startPosition = transform.position;
+
+        while (time < duration)
+        {
+            transform.position = Vector3.Lerp(startPosition, target,
+                time / duration);
+                
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = target;
+    }
 }
