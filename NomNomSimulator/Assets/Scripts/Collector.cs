49215ee2b7d3d15fca535b16c62756e41e54181b
danfupo03 <danfupo@outlook.com>
2023-11-23 09:58:25 -0600

    ...

--- NomNomSimulator/Assets/Scripts/Collector.cs
+++ NomNomSimulator/Assets/Scripts/Collector.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class Collector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Collector(int id)
     {
-        int randomXPosition = Mathf.RoundToInt(UnityEngine.Random.Range(0.0f, 20.0f));
-        int randomZPosition = Mathf.RoundToInt(UnityEngine.Random.Range(0.0f, 20.0f));
-
-        transform.position = new Vector3(randomXPosition, 0.6f, randomZPosition);
-
-        Vector3 currentPosition = transform.position;
+        // Aparecer en la posición inicial
+        transform.position = new Vector3(3, 0.6f, 13);
     }
 
     public void Move(int x, int z)
--- NomNomSimulator/Assets/Scripts/SimManager.cs
+++ NomNomSimulator/Assets/Scripts/SimManager.cs
@@ -4,5 +4,32 @@ using UnityEngine;
 
 public class SimManager : MonoBehaviour
 {
+    public Collector collector;
+    // public Explorer explorer;
+    public Warehouse warehouse;
+    public Food food;
+
+    public int totalSteps = 10;
+
+    void Start()
+    {
+        // Llamar a la función que inicializa la simulación
+        InitializeSimulation(); 
+    }
+
+    void Update()
+    {
+        // Llamar a la función que realiza los pasos de simulación en cada frame
+        SimulateStep();
+    }
+
+    void InitializeSimulation()
+    {
+        warehouse.Appearance(3, 13);
+    }
+
+    void SimulateStep()
+    {
         
+    }
 }
