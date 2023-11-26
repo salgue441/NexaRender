f5c04fe7587d168872a98b3bc251a2f261229011
danfupo03 <danfupo@outlook.com>
2023-11-25 19:18:12 -0600

    feat(Collector): Now the collector carries the waffle after grabbing it

--- NomNomSimulator/Assets/Scripts/Collector.cs
+++ NomNomSimulator/Assets/Scripts/Collector.cs
@@ -10,6 +10,7 @@ public class Collector : MonoBehaviour
     public AgentModel agent;
     private Animator animator;
     private bool isEating = false;
+    public GameObject wafflePrefab;
 
     // Constructor
     /// <summary>
@@ -89,12 +90,26 @@ public class Collector : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Waffle") && !isEating)
         {
+            float yOffset = 0.49f;
+            float xOffset = -0.25f;
+
+            Vector3 wafflePosition = new Vector3(transform.position.x + xOffset, transform.position.y + yOffset, transform.position.z);
+
+            float rotation = 90f;
+            Quaternion waffleRotation = Quaternion.Euler(rotation, 0, 0);
+
+            GameObject waffle = Instantiate(wafflePrefab, wafflePosition, waffleRotation, transform);
+
+            float scaleMultiplier = 0.5f;
+            waffle.transform.localScale = new Vector3(scaleMultiplier, scaleMultiplier, scaleMultiplier);
+
             Destroy(other.gameObject);
             animator.Play("Eat");
             isEating = true;
         }
         if (other.gameObject.CompareTag("Warehouse") && isEating)
         {
+            Destroy(transform.GetChild(6).gameObject);
             isEating = false;
         }
     }
